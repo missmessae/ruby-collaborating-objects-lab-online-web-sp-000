4dfc5369fe95444faf7153416f4a675f210f69f8
Annie Wiegel <annewiegel17@gmail.com>
2021-02-18 20:44:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,15 +13,15 @@ class Song
 
   def self.new_by_filename(filename)
     song = filename.split(" - ")[1]
-      artist = filename.split(" - ")[0]
-      new_song = self.new(song)
-      new_song.artist_name = artist
-      new_song
+    artist = filename.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
   end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    self.artist.add_song(self)
+    artist.add_song(self)
   end
 
 end
