571e5c2744333c344682eb9a5f1e140149a61b51
Annie Wiegel <annewiegel17@gmail.com>
2021-02-18 20:13:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,14 +14,14 @@ class Song
   def self.new_by_filename(filename)
     filename.split(" - ")
     filename[0] = @name
-    filename[1] = @artist
+    filename[1] = arist_name
     initialize(filename[0])
   end
 
 
-  def artist_name=
+  def artist_name
     Artist.find_or_create_by_name(@name)
-    
+
   end
 
 end
