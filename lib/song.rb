ccd01b8672d7432041ae599ba0551a8566c64d3a
Annie Wiegel <annewiegel17@gmail.com>
2021-02-18 20:22:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,8 @@ class Song
     initialize(@name)
   end
 
-  def artist_name=(@artist)
-    Artist.find_or_create_by_name(@artist)
+  def artist_name=(artist)
+    Artist.find_or_create_by_name(artist)
   end
 
 end
