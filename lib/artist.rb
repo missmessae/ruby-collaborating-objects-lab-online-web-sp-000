f41624d25b22e1089b8d48ac9a87492be62d0d86
Annie Wiegel <annewiegel17@gmail.com>
2021-02-18 20:26:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,12 +5,16 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+  end
+
+  def save
     @@all << self
   end
 
   def self.all
     @@all
   end
+
 
   def add_song(song)
     @songs << song
